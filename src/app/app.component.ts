c531a2568ba11984e9cc6243ae16342aa313b783
Nazar <nazaranddobriy@gmail.com>
2024-02-28 20:32:52 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'api-dashboard';
-}
+export class AppComponent {}
